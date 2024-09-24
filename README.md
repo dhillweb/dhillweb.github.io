0bc61e9799fb7752cbe66eba19012909aace5fc3
Daniel Hill <danielhill@magenta.de>
2024-09-25 01:10:36 +0200

    final

--- README.md
+++
@@ -1,1 +0,0 @@
-![](https://repository-images.githubusercontent.com/855971197/fb392328-bcff-415c-a3c0-11ebcdbacd0d)
--- app.vue
+++ app.vue
@@ -2,13 +2,6 @@
   <div>
     <NuxtParticles id="tsparticles" :options="options" @load="onLoad" />
 
-    <section>
-      <div class='air air1'></div>
-      <div class='air air2'></div>
-      <div class='air air3'></div>
-      <div class='air air4'></div>
-    </section>
-
 
   </div>
 </template>
@@ -87,86 +80,3 @@ const onLoad = (container: Container) => {
   setTimeout(() => container.play(), 2000)
 }
 </script>
-<style>
-body {
-  margin: 0;
-  background-color: black;
-}
-
-h1 {
-  color: aliceblue;
-  text-align: center;
-  top: 50%;
-  left: 50%;
-  position: absolute;
-}
-
-section {
-  position: relative;
-  width: 100%;
-  height: 100vh;
-  overflow: hidden;
-}
-
-section .air {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100px;
-  background: url(/wave.png);
-  background-size: 1000px 100px
-}
-
-section .air.air1 {
-  animation: wave 30s linear infinite;
-  z-index: 1000;
-  opacity: 1;
-  animation-delay: 0s;
-  bottom: 0;
-}
-
-section .air.air2 {
-  animation: wave2 15s linear infinite;
-  z-index: 999;
-  opacity: 0.5;
-  animation-delay: -5s;
-  bottom: 10px;
-}
-
-section .air.air3 {
-  animation: wave 30s linear infinite;
-  z-index: 998;
-  opacity: 0.2;
-  animation-delay: -2s;
-  bottom: 15px;
-}
-
-section .air.air4 {
-  animation: wave2 5s linear infinite;
-  z-index: 997;
-  opacity: 0.7;
-  animation-delay: -5s;
-  bottom: 20px;
-}
-
-@keyframes wave {
-  0% {
-    background-position-x: 1px;
-  }
-
-  100% {
-    background-position-x: 1000px;
-  }
-}
-
-@keyframes wave2 {
-  0% {
-    background-position-x: 1px;
-  }
-
-  100% {
-    background-position-x: -1000px;
-  }
-}
-</style>
